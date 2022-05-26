2f4e4f83e4712ebe26310509829da096ccbea9e8
YurchenkoAnn <anka.cyclik4321@gmail.com>
2022-05-27 00:07:18 +0500

    added "АХАХАА даешь конфликт"

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("АХАХАХАХА даешь конфликт!!!");
+        System.out.println("АХАХАХАХА даешь конфликт");
     }
 }
